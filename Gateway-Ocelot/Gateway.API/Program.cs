93ef61ccd9b180ef10dcbf306fc0c63424dc4c95
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2024-12-04 23:58:28 +0330

    use Ocelot For Access to downstream Api

--- Gateway-Ocelot/Gateway.API/Program.cs
+++ Gateway-Ocelot/Gateway.API/Program.cs
@@ -1,11 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", true, true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+        .AddJsonFile("ocelot.json")
+        .AddEnvironmentVariables();
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddOcelot();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +25,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseOcelot().Wait();
 
 app.UseHttpsRedirection();
 
