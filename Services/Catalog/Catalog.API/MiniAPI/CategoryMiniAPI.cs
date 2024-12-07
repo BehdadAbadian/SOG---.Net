6becdd0e97dd90010bf33c8de05c18e49f9a132b
Behdad BahramAbadian <Behdad.abadian@gmail.com>
2024-12-08 00:50:11 +0330

    add CatalogActionResult to miniapi

--- Services/Catalog/Catalog.API/MiniAPI/CategoryMiniAPI.cs
+++ Services/Catalog/Catalog.API/MiniAPI/CategoryMiniAPI.cs
@@ -1,5 +1,6 @@
 ﻿using Catalog.Application.CategoryCommandQuery.Command;
 using Catalog.Application.CategoryCommandQuery.Query;
+using Catalog.Appllication.Contract.Common;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Runtime.CompilerServices;
@@ -11,15 +12,32 @@ public static class CategoryMiniAPI
 {
     public static void AddMiniAPI(this WebApplication app)
     {
-        app.MapGet("/Category/getall", async (IMediator _mediator, PermissionClient _permission) =>
+        app.MapGet("/Category/getall", async Task<CatalogActionResult<List<GetAllCategoryQueryRespond>>> (IMediator _mediator, PermissionClient _permission) =>
         {
-            List<GetAllCategoryQueryRespond> b = new List<GetAllCategoryQueryRespond>();
+        
             var pCheck = await _permission.CheckAsync(new Catalog.API.Protos.CheckPermissionRequest { Role = "Admin" });
             if (pCheck.Success)
             {
-                return await _mediator.Send(new GetAllCategoryQuery());
+                var data = await _mediator.Send(new GetAllCategoryQuery());
+                return new CatalogActionResult<List<GetAllCategoryQueryRespond>>
+                {
+                    Data = data,
+                    IsSuccess = true,
+                    StatusCode = 200,
+                    Message = "Ok"
+                   
+                };
             }
-            else { return b; }
+            else 
+            { 
+                return new CatalogActionResult<List<GetAllCategoryQueryRespond>> 
+                {
+                    
+                    IsSuccess = false,
+                    StatusCode = 403,
+                    Message = "access to the requested resource is forbidden"
+                }; 
+            }
         });
 
         app.MapPost("/Category/add", async (IMediator _mediator, AddCategoryCommand command) =>
---
+++ Services/Catalog/Catalog.Appllication.Contract/Common/CatalogActionResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Appllication.Contract.Common;
+
+public class CatalogActionResult<T> where T : class
+{
+    public T Data { get; set; }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public int StatusCode { get; set; }
+}
